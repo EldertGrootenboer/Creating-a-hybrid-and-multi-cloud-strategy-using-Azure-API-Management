a1b6e099103936f807507fa9c546eae8049758da
Eldert Grootenboer <eldert@eldert.net>
2020-09-24 21:39:29 +0200

    Fixed template issues

--- assets/iac/1-deployment.ps1
+++ assets/iac/1-deployment.ps1
@@ -2,9 +2,13 @@
 #   1. Create a resource group
 #   2. Deploy Azure services
 
+########## Set before deployment, do not save to repository ##########
+$sqlAdminPassword = ConvertTo-SecureString "sj2#m222nndIN82#223@1" -AsPlainText -Force
+######################################################################
+
 # Update these according to the environment
 $subscriptionName = "Visual Studio Enterprise"
-$resourceGroupName = "rg-hybrid-multi-cloud-strategy"
+$resourceGroupName = "rg-hybrid-multi-cloud-strategy-2"
 $basePath = "C:\Users\elder\OneDrive\Sessions\Creating-a-hybrid-and-multi-cloud-strategy-using-Azure-API-Management"
 
 # Login to Azure
@@ -12,7 +16,7 @@ Get-AzSubscription -SubscriptionName $subscriptionName | Set-AzContext
 
 # Create the resource group and deploy the resources
 New-AzResourceGroup -Name $resourceGroupName -Location 'West Europe' -Tag @{CreationDate=[DateTime]::UtcNow.ToString(); Project="Creating a hybrid and multi-cloud strategy using Azure API Management"; Purpose="Session"}
-New-AzResourceGroupDeployment -Name "HybridMultiCloud" -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\assets\iac\azuredeploy.1.json"
+New-AzResourceGroupDeployment -Name "HybridMultiCloud" -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\assets\iac\azuredeploy.json" -administratorPassword $sqlAdminPassword
 
 # Optional for debugging, loops through each local file individually
 #Get-ChildItem "$basePath\assets\iac" -Recurse -Filter *.json | 
